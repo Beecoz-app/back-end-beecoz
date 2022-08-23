019a3db74f6d4ae17e78f2d05639454e756269a2
drakeira <thiago.p.dasilva2005@gmail.com>
2022-08-23 18:14:09 -0300

    feat: add publication repository

---
+++ src/repositories/Publication/PublicationRepository.ts
@@ -0,0 +1,50 @@
+import { PrismaClient ,Publication, Prisma, PrismaPromise } from "@prisma/client";
+const prisma = new PrismaClient()
+
+interface CreateDTO {
+    data: Publication
+}
+interface UpdateDTO {
+    id: number;
+    data: Publication
+}
+interface DeleteDTO {
+    id: number
+}
+
+class PublicationRepository {
+    async create({data}: CreateDTO): Promise<Publication> {
+        const publication = await prisma.publication.create({
+            data: {
+                ...data
+            }
+        })
+        return publication
+    }
+    async read(): Promise<PrismaPromise<Publication[]>> {
+        const publications = await prisma.publication.findMany()
+        return publications
+
+    }
+    async update({id, data}: UpdateDTO): Promise<Prisma.Prisma__LoginClient<Publication>> {
+        const newPublication = await prisma.publication.update({
+            where: {
+                id
+            },
+            data: {
+                ...data
+            }
+        })
+        return newPublication
+    }
+    async delete({id}: DeleteDTO): Promise<Prisma.Prisma__LoginClient<Publication>> {
+        const deletedPublication = await prisma.publication.delete({
+            where: {
+                id
+            }
+        })
+        return deletedPublication
+    }
+}
+
+export default new PublicationRepository()
