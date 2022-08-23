f94e719b09e7a456fecc3f57862b365cc0a5fb85
drakeira <thiago.p.dasilva2005@gmail.com>
2022-08-23 17:21:59 -0300

    feat: add client repository

---
+++ src/repositories/Client/ClientRepository.ts
@@ -0,0 +1,50 @@
+import { PrismaClient ,Client, PrismaPromise, Prisma } from "@prisma/client";
+const prisma = new PrismaClient()
+
+interface CreateDTO {
+    data: Client
+}
+interface UpdateDTO {
+    id: number;
+    data: Client
+}
+interface DeleteDTO {
+    id: number
+}
+
+class ClientRepository {
+    async create({data}: CreateDTO): Promise<Client> {
+        const client = await prisma.client.create({
+            data: {
+                ...data
+            }
+        })
+        return client
+    }
+    async read(): Promise<PrismaPromise<Client[]>> {
+        const clients = await prisma.client.findMany()
+        return clients
+
+    }
+    async update({id, data}: UpdateDTO): Promise<Prisma.Prisma__LoginClient<Client>> {
+        const newClient = await prisma.client.update({
+            where: {
+                id
+            },
+            data: {
+                ...data
+            }
+        })
+        return newClient
+    }
+    async delete({id}: DeleteDTO): Promise<Prisma.Prisma__LoginClient<Client>> {
+        const deletedClient = await prisma.client.delete({
+            where: {
+                id
+            }
+        })
+        return deletedClient
+    }
+}
+
+export default new ClientRepository()
