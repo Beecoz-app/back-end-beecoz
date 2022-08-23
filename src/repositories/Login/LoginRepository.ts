26bbbe1ae5e22f211f8e8d51a720c63ea732bdb9
drakeira <thiago.p.dasilva2005@gmail.com>
2022-08-23 17:14:25 -0300

    feat: add login repository

---
+++ src/repositories/Login/LoginRepository.ts
@@ -0,0 +1,48 @@
+import { PrismaClient ,Login, Prisma, PrismaPromise } from "@prisma/client";
+const prisma = new PrismaClient()
+
+interface CreateDTO {
+    data: Login
+}
+interface UpdateDTO {
+    id: number;
+    data: Login
+}
+interface DeleteDTO {
+    id: number
+}
+
+class LoginRepository {
+    async create({data}: CreateDTO): Promise<Login> {
+        const login = await prisma.login.create({
+            data: {
+                ...data
+            }
+        })
+        return login
+    }
+    async read(): Promise<PrismaPromise<Login[]>> {
+        const logins = await prisma.login.findMany()
+        return logins
+
+    }
+    async update({id, data}: UpdateDTO): Promise<Prisma.Prisma__LoginClient<Login>> {
+        const newLogin = await prisma.login.update({
+            where: {
+                id
+            },
+            data: {
+                ...data
+            }
+        })
+        return newLogin
+    }
+    async delete({id}: DeleteDTO): Promise<Prisma.Prisma__LoginClient<Login>> {
+        const deletedLogin = await prisma.login.delete({
+            where: {
+                id
+            }
+        })
+        return deletedLogin
+    }
+}
