4fd02fb08281f4e304809b2d11d76e8e277dceb1
drakeira <thiago.p.dasilva2005@gmail.com>
2022-09-06 11:41:07 -0300

    feat: add work repository interface

---
+++ src/interfaces/repositories/Work/IWorkRepository.ts
@@ -0,0 +1,9 @@
+import { Prisma, PrismaPromise, Work } from "@prisma/client";
+import { WorkRepositoryCreateDTO, WorkRepositoryDeleteDTO, WorkRepositoryUpdateDTO } from "../../DTOs/repositories/Work/WorkRepositoryDTO";
+
+export interface IWorkRepository {
+    create({data}: WorkRepositoryCreateDTO): Promise<Work>;
+    read(): Promise<PrismaPromise<Work[]>>;
+    update({id, data}: WorkRepositoryUpdateDTO): Promise<Prisma.Prisma__LoginClient<Work>>;
+    delete({id}: WorkRepositoryDeleteDTO): Promise<Prisma.Prisma__LoginClient<Work>>;
+}
