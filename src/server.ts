75d9ac8d8d78d54832145f3f1bbacfc8aa32510e
drakeira <thiago.p.dasilva2005@gmail.com>
2022-09-17 20:43:05 -0300

    feat: add cors

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,11 @@
 import express from 'express'
+import cors from 'cors'
 import { routes } from './routes/routes'
 
 const app = express()
 
 app.use(express.json())
+app.use(cors())
 app.use(routes)
 
 app.listen(4444, () => console.log('server on 4444 port!'))
