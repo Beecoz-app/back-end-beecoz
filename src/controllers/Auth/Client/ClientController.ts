8e7de0cd4d78d20d0eeb871b3f33971835db7c93
drakeira <thiago.p.dasilva2005@gmail.com>
2022-09-18 08:24:36 -0300

    fix: fix alguma coisa ai

--- src/controllers/Auth/Client/ClientController.ts
+++ src/controllers/Auth/Client/ClientController.ts
@@ -4,7 +4,7 @@ import ClientProfileRepository from "../../../repositories/Client/ClientProfile/
 import ClientRepository from "../../../repositories/Client/ClientRepository";
 import LoginRepository from "../../../repositories/Login/LoginRepository";
 import TypeUserRepository from "../../../repositories/TypeUser/TypeUserRepository";
-import { hashPassword} from "../../../utils/password";
+import { hashPassword } from "../../../utils/password";
 import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
 
@@ -19,57 +19,78 @@ class AuthClientController {
       gender,
       bornDate,
       cpf,
-      biography
-    }: Client & { email: string; password: string, cellNumber: number | null, biography: string } = req.body;
-      
+      biography,
+    }: Client & {
+      email: string;
+      password: string;
+      cellNumber: string;
+      biography: string;
+    } = req.body;
 
-    const login = await LoginRepository.create({ data: { email, password: await hashPassword(password), cellNumber } });
-    const profile = await ClientProfileRepository.create({ data: { biography } })
-    const typeId = await TypeUserRepository.findByLevel({ level: gender === 'Female' ? 'Queen' : 'Beginner' })
 
+    const login = await LoginRepository.create({
+      data: { email, password: await hashPassword(password), cellNumber },
+    });
 
-    const client = await ClientRepository.create({ data: { name, lastName, gender, bornDate: new Date(bornDate), cpf, loginId: login.id, profileId: profile.id, typeId } })
+    const profile = await ClientProfileRepository.create({
+      data: { biography },
+    });
+    const typeId = await TypeUserRepository.findByLevel({
+      level: gender === "Female" ? "Queen" : "Beginner",
+    });
+
+    const client = await ClientRepository.create({
+      data: {
+        name,
+        lastName,
+        gender,
+        bornDate: new Date(),
+        cpf,
+        loginId: login.id,
+        profileId: profile.id,
+        typeId,
+      },
+    });
 
     const token = jwt.sign({ id: client.id }, String(process.env.AUTH_SECRET), {
       expiresIn: 86400,
     });
-    
 
-    return res.json({ client, token })
+    return res.json({ client, token });
   }
 
-  async read (req: Request, res: Response) {
+  async read(req: Request, res: Response) {
     const { id } = req.params;
     const parsedId = Number(id);
 
     const client = await ClientRepository.findClientById({ id: parsedId });
 
-    return res.json(client)
+    return res.json(client);
   }
 
-  async readById (req: Request, res:Response) {
+  async readById(req: Request, res: Response) {
     const { id } = req.params;
     const parsedId = Number(id);
 
     const client = await ClientRepository.findClientById({ id: parsedId });
 
-    return res.json(client)
+    return res.json(client);
   }
 
   async delete(req: Request, res: Response) {
     const { id } = req.params;
     const parsedId = Number(id);
-    
-    const clientExists = await ClientRepository.findClientById({ id: parsedId });
+
+    const clientExists = await ClientRepository.findClientById({
+      id: parsedId,
+    });
     if (!clientExists) {
       return res.status(400).json({ message: "Client not found" });
     }
-  
+
     await ClientRepository.delete({ id: parsedId });
 
-    return res
-      .status(200)
-      .json({ message: "Client deleted successfully" });
+    return res.status(200).json({ message: "Client deleted successfully" });
   }
 
   async update(req: Request, res: Response) {
@@ -81,31 +102,36 @@ class AuthClientController {
       password,
       cellNumber,
       lastName,
-    }: Client & { email: string; password: string, cellNumber: number | null } = req.body;
+    }: Client & { email: string; password: string; cellNumber: string } =
+      req.body;
 
-    const clientExists = await ClientRepository.findClientById({ id: parsedId });
+    const clientExists = await ClientRepository.findClientById({
+      id: parsedId,
+    });
     if (!clientExists) {
       return res.status(400).json({ message: "Client not found" });
     }
 
-    
-    const login = await LoginRepository.update({ id: clientExists.loginId, data: { email, password: await hashPassword(password), cellNumber } });
-    const client = await ClientRepository.update({ id: parsedId, data: { name, lastName, loginId: login.id } })
+    const login = await LoginRepository.update({
+      id: clientExists.loginId,
+      data: { email, password: await hashPassword(password), cellNumber },
+    });
+    const client = await ClientRepository.update({
+      id: parsedId,
+      data: { name, lastName, loginId: login.id },
+    });
 
-    return res.json({ client })
+    return res.json({ client });
   }
 
-    async show (req: Request, res: Response) {
-      const { id } = req.params;
-      const parsedId = Number(id);
+  async show(req: Request, res: Response) {
+    const { id } = req.params;
+    const parsedId = Number(id);
 
-      const client = await ClientRepository.findClientById({ id: parsedId });
+    const client = await ClientRepository.findClientById({ id: parsedId });
 
-      return res.json(client)
-    }
-
-  
+    return res.json(client);
+  }
 }
-
 
 export default new AuthClientController();
