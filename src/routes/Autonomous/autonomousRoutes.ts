5b57757cab0717cec021ffaa5a72ba3d3624e45d
drakeira <thiago.p.dasilva2005@gmail.com>
2022-10-08 18:23:17 -0300

    feat: add autonomous routes

---
+++ src/routes/Autonomous/autonomousRoutes.ts
@@ -0,0 +1,5 @@
+import { Router } from "express";
+
+const autonomousRoutes = Router()
+
+export {autonomousRoutes}
--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -1,6 +1,7 @@
 import { Router } from "express";
 import { authenticateToken } from "../middleware/authenticateToken";
 import { authRoutes } from "./Auth/authRoutes";
+import { autonomousRoutes } from "./Autonomous/autonomousRoutes";
 import { interestRoutes } from "./Interest/interestRoutes";
 import { publicationRoutes } from "./Publication/publicationRoutes";
 import { serviceTypeRoutes } from "./ServiceType/serviceTypeRoutes";
@@ -8,6 +9,7 @@ import { serviceTypeRoutes } from "./ServiceType/serviceTypeRoutes";
 const routes = Router()
 
 routes.use('/auth', authRoutes)
+routes.use('/autonomous', authenticateToken, autonomousRoutes)
 routes.use('/service_type', serviceTypeRoutes)
 routes.use('/publication', authenticateToken , publicationRoutes)
 routes.use('/interest', authenticateToken ,interestRoutes)
